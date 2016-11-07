12915f05dffd3a7f8680a3a849d9b88e2945326b
trashoverride <jr.overhead@gmail.com>
2016-11-07 16:13:45 -0200

    Adicionando arquivos que  funcionam

--- Linux/return_libc/teste.c
+++ Linux/return_libc/teste.c
@@ -1,25 +1,34 @@
-#include<stdio.h>
+/*
+  antes de compilar: sudo echo "0" > /proc/sys/kernel/randomize_va_space
+  compile : gcc vuln.c -o vuln -zexecstack -fno-stack-protector -g
+  crash : ./vuln `perl -e 'print   "A"x76 . "\x61\x61\x61\x61"'`
+  Autor : BitKiller 
 
-char j[]="/bin/ls";
+*/ 
 
-void func_bug(char *a)
+#include <stdio.h> 
+#include <string.h>  
+
+void func_bug(char * ptr) 
+
+{ 
+  char a [64];
+	strcpy(a,ptr); 
+
+} 
+
+
+int  main(int argc, char * argv[]) 
 {
-	char buff[10];
-
-	strcpy(buff,a);
-
-
-}
-
-
-int main(int argc, char * argv[])
-{
-
-		
+	if(argc < 1)
+	{
+		printf("use:%s <string>\n", argv[1]);
+		return 1;
+	}
+	printf("inicio\n");
 	func_bug(argv[1]);
+	printf("fim\n");
 
-
-
-
+	return 1;
 
 }
